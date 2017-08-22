ba698ebebd9057709431dbe515a9764a1844ea38
ilya-wolf <ilua-wolf@yandex.ru>
2017-08-22 23:23:38 +0300

    edit 1 Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,12 @@
 public class Calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate.....");
-		
+		/*проводится сложение двух чиселенных параметров полученных  через массив функции main при запуске программы --  тоесть 
+		"java Calculate 2 3" */
+	int first = Integer.valueOf(arg[0]);/*первое число из массива*/
+	
+	int second = Integer.valueOf(arg[1]);/*второе число из массива*/
+	int sum = first+second;
+	System.out.println("Sum " +sum);
 	}
 }
